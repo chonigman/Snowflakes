ceb6739bd2f08ccef8ff0bdbf7d68d821c379a8e
KingWalrus <chonigman@gmail.com>
2016-12-15 14:17:47 -0800

    Add circle at center and lines connecting to vertices

--- polygon.js
+++ polygon.js
@@ -1,6 +1,7 @@
 function Polygon(x, y, radius, num_sides) {
   this.x = x;
   this.y = y;
+  this.pos = createVector(x, y);
   this.r = radius;
   this.n = num_sides;
   this.vertices = [];
@@ -12,12 +13,21 @@ function Polygon(x, y, radius, num_sides) {
     this.vertices.push({'x': sx, 'y': sy});
   }
 
+  var circle_r = random(0, this.r);
+
   this.draw = function() {
+
     beginShape();
     for (var i = 0; i < this.vertices.length; i++){
       vertex(this.vertices[i]['x'], this.vertices[i]['y']);
     }
     endShape(CLOSE);
+
+    for (var i = 0; i < this.vertices.length; i++){
+        line(this.x, this.y, this.vertices[i]['x'], this.vertices[i]['y']);
+    }
+    
+    ellipse(this.x, this.y, circle_r, circle_r);
   }
 
 }
