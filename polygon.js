5e579ecd87334db02f874ca85d2116f4e3ba87af
KingWalrus <chonigman@gmail.com>
2016-12-16 11:33:57 -0800

    Tweak some things

--- polygon.js
+++ polygon.js
@@ -3,7 +3,7 @@ function Polygon(x, y, radius, num_sides, is_center) {
     this.y = y;
     this.pos = createVector(x, y);
     this.r = radius;
-    this.max_r = random(radius, 10*radius);
+    this.max_r = random(radius, 2*radius);
     this.n = num_sides;
     this.vertices = [];
     var angle = TWO_PI / this.n;
@@ -12,6 +12,8 @@ function Polygon(x, y, radius, num_sides, is_center) {
     this.growing = true;
     var rotation = [0, .25, .5, .75, 1, 1.25, 1.5, 1.75, 2];
     this.count = 0;
+    this.center_color = random(palette);
+    this.second_color = random(palette);
     if (is_center){
         var offset = random(rotation) * angle;
         for (var a = offset; a < TWO_PI+offset; a += angle) {
@@ -29,30 +31,30 @@ function Polygon(x, y, radius, num_sides, is_center) {
 
 
     this.display = function() {
-
         beginShape();
         for (var i = 0; i < this.vertices.length; i++){
             var vert = this.vertices[i];
             vertex(vert.x, vert.y);
         }
         endShape(CLOSE);
-
         if (this.is_center){
             for (i = 0; i < this.vertices.length; i++){
                 vert = this.vertices[i];
                 var d = vert.copy().sub(this.pos).normalize();
-                if (this.pos.dist(vert) < 200){
-                    var v = vert.add(d.mult(this.r/10));
+                if (this.pos.dist(vert) < 100){//random(50, 100)){
+                    var v = vert.add(d.mult(this.r/2));
+                    // inner lines
                     line(this.x, this.y, v.x, v.y); 
                 } else if (frameCount < 100) {
                     var v = vert.copy().add(d.mult(frameCount));
+                    // extending lines
                     line(this.x, this.y, v.x, v.y); 
                 } else {
                     this.growing = false;
                 }
             }
             ellipse(this.x, this.y, circle_r, circle_r);
-        } else if (this.count < 200) {
+        }         else if (this.count < random(50, 100)) {
             for (i = 0; i < this.vertices.length; i++){
                 vert = this.vertices[i];
                 var d = vert.copy().sub(this.pos).normalize();
