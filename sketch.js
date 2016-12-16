501a2c12d100aa40fac26ff28bd0b66bcf2ebd73
KingWalrus <chonigman@gmail.com>
2016-12-16 08:35:12 -0800

    Update sketch

--- sketch.js
+++ sketch.js
@@ -1,45 +1,70 @@
 var palette;
 var bg_color;
+var s_color;
 var X_AXIS = 0;
 var Y_AXIS = 1;
 var center_radius;
 var center;
+var flakes;
+var startup;
+
 
 function setup() {
-  createCanvas(720, 720);
-  palette = loadPalette();
-  color1 = random(palette);
-  color2 = random(palette);
-  linearGradient(0, 0, width, height, color1, color2, Y_AXIS);
-  center_radius = int(random(0, width*.10));
-  center = Polygon(width/2, height/2, center_radius, 6);
+    createCanvas(720, 720);
+    palette = loadPalette();
+    color1 = random(palette);
+    color2 = random(palette);
+    s_color = random(palette);
+    linearGradient(0, 0, width, height, color1, color2, Y_AXIS);
+    center_radius = int(random(width*.05, width *.10));
+    center = new Polygon(width/2, height/2, center_radius, 6, true);
+    flakes = makeFlakes(center, center_radius);
+    startup = true;
+    //frameRate(10);
 }
 
 function draw() {
-  //background(bg_color);
-  stroke(255);
-  noFill();
-  center.draw();
-  //drawCircle(width / 2, height / 2, 300);
+    noFill();
+    stroke(s_color);
+    center.display();
+    if(center.growing == false){
+        for(let f of flakes){
+            f.display();
+        }
 
+    }
 }
 
 function linearGradient(x, y, w, h, c1, c2, axis){
-  noFill();
-  if (axis == Y_AXIS){
-    for (var i = y; i <= y + h; i++){
-      var inter = map(i, y, y + h, 0, 1);
-      var c = lerpColor(c1, c2, inter);
-      stroke(c);
-      line(x, i, x + w, i);
+    noFill();
+    if (axis == Y_AXIS){
+        for (var i = y; i <= y + h; i++){
+            var inter = map(i, y, y + h, 0, 1);
+            var c = lerpColor(c1, c2, inter);
+            stroke(c);
+            line(x, i, x + w, i);
+        }
+    } else if (axis == X_AXIS){
+        for(i = x; i <= x + w; i++){
+            inter = map(i, x, x + w, 0, 1);
+            c = lerpColor(c1, c2, inter);
+            stroke(c);
+            line(i, y, i, y + h);
+        }
     }
-  } else if (axis == X_AXIS){
-    for(var i = x; i <= x + w; i++){
-      var inter = map(i, x, x + w, 0, 1);
-      var c = lerpColor(c1, c2, inter);
-      stroke(c);
-      line(i, y, i, y + h);
+
+}
+
+function makeFlakes(center, center_radius){
+    var polys = [];
+    var vertices = center.getVertices();
+    var r = random(10, center_radius-5);
+    var sides = random([3, 5, 6]);
+    for(var i = 0; i < 6; i++){
+        var vert = vertices[i];
+        var p = new Polygon(vert.x, vert.y, r, sides, false);
+        polys.push(p);
     }
-  }
-  
+    startup = false;
+    return polys;
 }
