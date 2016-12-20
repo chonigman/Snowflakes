7ea7e5844446d17a7202d834c38fd58b86320fe6
KingWalrus <chonigman@gmail.com>
2016-12-19 22:34:50 -0800

    Have amaaziograph style mirroring and rotation working with input

--- sketch.js
+++ sketch.js
@@ -4,42 +4,148 @@ var X_AXIS = 0;
 var Y_AXIS = 1;
 var center_radius;
 var center;
+var angle; 
+var clickList = [];
+var clickMove = [];
+var clickStart = null;
+var clickEnd = null;
+var rot = 0;
+var pos;
 
 function setup() {
   createCanvas(720, 720);
   palette = loadPalette();
+  randomSeed(13);
   color1 = random(palette);
   color2 = random(palette);
+  color3 = random(palette);
   linearGradient(0, 0, width, height, color1, color2, Y_AXIS);
   center_radius = int(random(0, width*.25));
-  center = Polygon(width/2, height/2, center_radius, 6);
+  angle = 45;
+  rot = 0;
+  pos = createVector(width/2, height/2);
+  //center = Polygon(width/2, height/2, center_radius, 6);
 }
 
 function draw() {
-  //background(bg_color);
-  stroke(255);
+  linearGradient(0, 0, width, height, color1, color2, Y_AXIS);
+  stroke(color3);
   noFill();
-  center.draw();
-  //drawCircle(width / 2, height / 2, 300);
+  //print(pos);
+  push();
+  translate(pos.x, pos.y);
+if(clickStart !== null){
+      var mpos = getMousePos();
+
+      print(getMousePos());
+      line(clickStart.x, clickStart.y, mpos.x, mpos.y);
+  }
+  for(var i = 0; i < 8; i++){
+      rotate(radians(angle*i));
+      //line(0, 0, width/2, 0);
+      for(var j = 0; j < 10; j++){
+          //ellipse(20+20*j, 0, 10+j, 10+j);
+      }
+      for(j = 0; j < 5; j++){
+          //line(0, 0, 300, 10*j);
+      }
+      for(j = 0; j < 5; j++){
+          //line(20*j, 20*j, 20*j, -20*j);
+      }
+      if (mouseIsPressed){
+      //    var mpos = getMousePos();
+      //    var pmpos = getMousePos(true);
+      //    var lpos = createVector(lerp(pmpos.x, mpos.x, .5), lerp(pmpos.y, mpos.y, .5));
+      //    line(pmpos.x, pmpos.y, lpos.x, lpos.y); 
+      }
+      //if(clickStart !== null){
+      //  var mpos = getMousePos();
+      // line(clickStart.x, clickStart.y, mpos.x, mpos.y);
+      //}
+      //point(mpos.x, mpos.y);
+      if ( keyIsPressed == false){
+      for (j = 0; j < clickList.length; j++){
+          var l = clickList[j];
+          line(l.clickStart.x, l.clickStart.y, l.clickEnd.x, l.clickEnd.y);
+          //line(l.clickStart.x, -l.clickStart.y, l.clickEnd.x, -l.clickEnd.y);
+//          beginShape();
+//          for(var c = 0; c < l.clickMove.length; c++){
+//          //      vertex(l.clickMove[c].x, l.clickMove[c].y);
+//          }
+//
+//          endShape();
+
+          //clickList.splice(j, 1);
+
+      }
+      } else{
+       for (j = 0; j < clickList.length; j++){
+          var l = clickList[j];
+           push();
+           translate(l.clickStart.x, l.clickStart.y);
+           rotate(radians(rot%360));
+            var d = p5.Vector.sub(l.clickStart, l.clickEnd);
+          line(0, 0, d.x, d.y);
+          //line(0, -l.clickStart.y, -l.clickEnd.x, -l.clickEnd.y);
+          rot += .05;
+          pop();
+       }
+ 
+      }
+
+
+  }
+
+  
+  pop();
 
 }
 
+function mousePressed(){
+    var mpos = getMousePos(); 
+    clickStart = createVector(mpos.x, mpos.y);
+}
+
+function mouseDragged(){
+    var mpos = getMousePos();
+    var pmpos = getMousePos(true);
+    if (pmpos.dist(mpos) > 0){
+        clickMove.push(getMousePos());
+    }
+}
+
+function mouseReleased(){
+    clickEnd = getMousePos(); 
+    clickList.push({'clickStart': clickStart, 'clickEnd': clickEnd, 'clickMove': clickMove})
+        clickStart = null;
+}
+
+function getMousePos(previous){
+    if(previous == null){
+        var mpos = p5.Vector.sub(createVector(mouseX, mouseY), pos);
+    }else if (previous == true) {
+        var mpos = p5.Vector.sub(createVector(pmouseX, pmouseY), pos);
+    }
+    return mpos;
+}
+
+
 function linearGradient(x, y, w, h, c1, c2, axis){
-  noFill();
-  if (axis == Y_AXIS){
-    for (var i = y; i <= y + h; i++){
-      var inter = map(i, y, y + h, 0, 1);
-      var c = lerpColor(c1, c2, inter);
-      stroke(c);
-      line(x, i, x + w, i);
+    noFill();
+    if (axis == Y_AXIS){
+        for (var i = y; i <= y + h; i++){
+            var inter = map(i, y, y + h, 0, 1);
+            var c = lerpColor(c1, c2, inter);
+            stroke(c);
+            line(x, i, x + w, i);
+        }
+    } else if (axis == X_AXIS){
+        for(var i = x; i <= x + w; i++){
+            var inter = map(i, x, x + w, 0, 1);
+            var c = lerpColor(c1, c2, inter);
+            stroke(c);
+            line(i, y, i, y + h);
+        }
     }
-  } else if (axis == X_AXIS){
-    for(var i = x; i <= x + w; i++){
-      var inter = map(i, x, x + w, 0, 1);
-      var c = lerpColor(c1, c2, inter);
-      stroke(c);
-      line(i, y, i, y + h);
-    }
-  }
-  
+
 }
